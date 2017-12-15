c922409d6baaa7f9838bbe4d20ec581a000ba8d1
John Parejko <parejkoj@uw.edu>
2017-12-14 17:12:09 -0800

    Make jointcal script parallel across filters

--- slurm/jointcal-process.py
+++ slurm/jointcal-process.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Generate slurm files for multiple jointcal jobs.`
+Generate slurm files for multiple jointcal jobs, parallelized across filters.
 """
 from __future__ import print_function
 
@@ -8,31 +8,51 @@ import os
 import sqlite3
 import subprocess
 
+import lsst.utils
+
 base_slurm = """#!/bin/bash -l
 
 #SBATCH -p debug
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=1
 #SBATCH --time=1440
 #SBATCH -J {name}
-#SBATCH --output=logs/{name}-%j.log
-#SBATCH --error=logs/{name}-%j.err
 
 source /software/lsstsw/stack/loadLSST.bash
-setup -r /scratch/parejkoj/jointcal/
-setup -jkr /scratch/parejkoj/obs_subaru
-setup -jkr /scratch/parejkoj/daf_persistence
+setup -r /project/parejkoj/stack/jointcal/
 
-srun jointcal.py {datadir} --rerun={rerun} -C={config} --id ccd={ccd} filter={filt} tract={tract} field={field} visit={visit} --longlog --no-versions
+pids=()
+
+{cmd}
+
+for pid in ${{pids[*]}};
+do
+    echo "Waiting: $pid"
+    wait $pid  # Wait on all PIDs, this returns 0 if ANY process fails
+done
+
 """
 
-sqlitedir = '/scratch/hchiang2/parejko/'
+base_cmd = ("srun --output=/project/parejkoj/DM-11783/logs/{name}_{filt}-%J.log"
+            "jointcal.py {datadir} --rerun={rerun} -C={config}"
+            "--id ccd={ccd} filter={filt} tract={tract} field={field} visit={visit}"
+            "--longlog --no-versions &\n"
+            "pids+=($!)  # Save PID of this background process")
+
+basename = 'jointcal'
+
+pkgdir = lsst.utils.getPackageDir('jointcal_compare')
+
+root = '/project/parejkoj/DM-11783'
+sqlitedir = os.path.join(root, 'tract-visit')
 datadir = '/datasets/hsc/repo'
-# config = 'jointcalConfig-hsc-astrometry.py'
+outdir = os.path.join(root, basename)
 config = 'jointcalConfig-hsc.py'
+config = os.path.join(pkgdir, 'config', basename+'Config.py')
 rerun = 'DM-10404/SFM:private/parejkoj/DM-11785'
 
 ccd = "0..8^10..103"
+
+call = True
 
 
 def find_visits(cursor, tract, filt, field):
@@ -43,21 +63,26 @@ def find_visits(cursor, tract, filt, field):
     return '^'.join(str(x[0]) for x in result)
 
 
-def generate_one(output, field, tract, filt, ccd, cursor, call=True):
+def generate_one(field, tract, filters, ccd, cursor, call=True):
     """Generate and execute a slurm script."""
 
-    visit = find_visits(cursor, tract, filt, field)
-
-    fmtstr = dict(field=field, tract=tract, filt=filt, ccd=ccd, visit=visit,
-                  datadir=datadir, rerun=rerun, config=config)
-    name = "jointcal-{field}_{tract}_{filt}".format(**fmtstr)
+    output = os.path.join(outdir, str(tract))
+    fmtstr = dict(output=output, field=field, tract=tract, ccd=ccd, rerun=rerun,
+                  datadir=datadir, config=config, ntasks=len(filters))
+    name = basename + "-{field}_{tract}".format(**fmtstr)
     fmtstr['name'] = name
 
-    outlog = open('slurm-logs/{name}.log'.format(**fmtstr), 'w')
+    cmd_list = []
+    for filt in filters:
+        visit = find_visits(cursor, tract, filt, field)
+        cmd_list.append(base_cmd.format(**fmtstr, filt=filt, visit=visit))
+    cmd = '\n'.join(cmd_list)
 
-    filename = 'scripts/{name}.sl'.format(**fmtstr)
+    outlog = open(os.path.join(root, 'slurm-logs/{name}.log'.format(**fmtstr), 'w'))
+
+    filename = os.path.join(root, 'scripts/{name}.sl'.format(**fmtstr))
     with open(filename, 'w') as outfile:
-        outfile.write(base_slurm.format(**fmtstr))
+        outfile.write(base_slurm.format(cmd=cmd, **fmtstr))
     print('Generated:', filename)
     if call:
         cmd = 'sbatch %s'%filename
@@ -66,23 +91,19 @@ def generate_one(output, field, tract, filt, ccd, cursor, call=True):
 
 
 # deep data:
-output = 'jointcal-hsc-deep'
 tract = 9813
 field = "SSP_UDEEP_COSMOS"
-filters = ['HSC-Y', 'HSC-Z', 'HSC-I', 'HSC-R', 'HSC-G', 'NB0921']
+filters = ['HSC-Y', 'HSC-Z', 'HSC-I', 'HSC-R', 'HSC-G']
 conn = sqlite3.connect(os.path.join(sqlitedir, 'dbUDEEP.sqlite3'))
 cursor = conn.cursor()
-for filt in filters:
-    generate_one(output, field, tract, filt, ccd, cursor, call=True)
+generate_one(field, tract, filters, ccd, cursor, call=call)
 
 # wide data:
-output = 'jointcal-hsc-wide'
-tracts = [8521, 8522, 8523, 8524, 8525, 9558, 9559, 9560, 9561, 9371, 9372, 9373, 9374, 9693, 9694, 9695, 9697, 9698, 15831, 15832, 16009, 16010]
+tracts = [8521, 8522, 8523, 8524, 8525, 9558, 9559, 9560, 9561, 9371, 9372,
+          9373, 9374, 9693, 9694, 9695, 9697, 9698, 15831, 15832, 16009, 16010]
 field = "SSP_WIDE"
 filters = ['HSC-Y', 'HSC-Z', 'HSC-I', 'HSC-R', 'HSC-G']
 conn = sqlite3.connect(os.path.join(sqlitedir, 'dbWIDE.sqlite3'))
 cursor = conn.cursor()
 for tract in tracts:
-    for filt in filters:
-        generate_one(output, field, tract, filt, ccd, cursor, call=True)
-
+    generate_one(field, tract, filters, ccd, cursor, call=call)
