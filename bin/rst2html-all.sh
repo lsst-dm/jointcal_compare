80ad5d82e5e7fe4e3af92de8337c585d7e1e20ad
John K. Parejko <parejkoj@uw.edu>
2018-07-29 23:09:39 -0700

    Document and cleanup rst2html script

--- bin/rst2html-all.sh
+++ bin/rst2html-all.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# found here:
+#
+# Convert a bunch of rst files to html.
+# Run this with the first argument as the directory containing the .rst files
+# you want to convert. The results will go into that dir/html/.
+#
+# Adapted from here:
 # https://superuser.com/questions/742836/batch-rst2html-conversion-in-a-bash-script
 
 directory=$1
 
-for i in $(find $directory -type f -name \*.rst)
+for i in $(find "$directory" -type f -name "*.rst")
 do
     RST_FILE="$i"
     HTML_BASE=$(basename $RST_FILE)
